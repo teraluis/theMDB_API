3911b393d49f76c16b7dc285d6ede873d6e9cfbf
Luis Manresa <luis.manresa@fabrick-io.com>
2021-04-02 01:06:45 +0200

    title might not be unique

--- modules/movies/model/movieModel.js
+++ modules/movies/model/movieModel.js
@@ -11,7 +11,7 @@ let MovieSchema = new Schema({
     },
     title: {
         type: String,
-        unique: true,
+        unique: false,
         required: [true, 'title is mandatory']
     },
     image: {
